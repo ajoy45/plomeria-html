7d7fac6bef89ea466cf2b43131a095c1caf1bbd3
ajoy45 <ajoypust@gmail.com>
2023-12-21 17:38:48 +0600

    service area section completed

--- js/custom.js
+++ js/custom.js
@@ -5,7 +5,34 @@ $(document).ready(function () {
         slidesToShow: 3,
         slidesToScroll: 1,
         speed: 300,
-        autoplay: true,
-        arrows:true
+        // autoplay: true,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 1450,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                }
+            },
+
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            }
+
+        ]
+
     });
 });
