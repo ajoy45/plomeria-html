8812bf997d0199056d3dfab7d80b3da3de8baf09
ajoy45 <ajoypust@gmail.com>
2023-12-21 09:13:21 +0600

    service-area card added

--- js/custom.js
+++ js/custom.js
@@ -1,59 +1,10 @@
 $(document).ready(function () {
-
-    // const btn = document.querySelector(".hamburger");
-    // const icon = document.querySelector(".fa-bars");
-    // const navBar = document.querySelector('.nav-links')
-    // btn.addEventListener('click', () => {
-    //     if (icon.classList.contains("fa-bars")) {
-    //         icon.classList.replace("fa-bars", "fa-times");
-    //         navBar.classList.add('header-active');
-    //         navBar.style.display = "block";
-    //     }
-    //     else {
-    //         icon.classList.replace("fa-times", "fa-bars");
-    //         navBar.classList.add = "header-active";
-    //         navBar.style.display = "none";
-
-    //     }
-    // })
-
-
-    $('.testimonial-card-wrapper').slick({
+    $('.service-area-slider').slick({
+        infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
+        speed: 300,
         autoplay: true,
-        autoplaySpeed: 1000, // Set the duration between slides in milliseconds
-        speed: 3000,
-        infinite: true,
-        swipe: true,
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-
-
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 2,
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-
-        ]
-
-
-
+        arrows: true
     });
 });
